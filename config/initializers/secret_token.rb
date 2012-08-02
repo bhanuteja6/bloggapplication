5518173b03674f16943d27b88a2a003a8bd84f61
bhanuteja6 <bhanuteja6@gmail.com>
2012-08-02 16:39:38 +0530

    hai

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railex::Application.config.secret_token = '744e6d77c5012390567eb8294f86fd189933f97ccb227b343f7cb3cd5781879d6cc337e55e0cee9999733d8adfc1b2b39927e84998a2952e8b66f05d1654a0f9'
